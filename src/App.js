6510eab71c404ab451422fbb5cfea68ef73976f1
Hosamov <hosamov@hotmail.com>
2021-07-14 07:07:28 -0700

    updated using props and added Card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+// Import custom components:
+
+import Expenses from './components/Expenses';
+
+// App function to handle all components:
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 3, 14)
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 799.49,
+      date: new Date(2021, 2, 12)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28)
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12)
+    },
+  ];
+
+  // JSX (JavaScript XML) code below:
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses items={expenses}/>
     </div>
   );
 }
 
-export default App;
+export default App; //export so it may be imported
--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
-import ReactDOM from 'react-dom';
+/*
+* NOTE: The code in index.js is transformed before being delivered to the browser.
+*       That's why you can do things like import a css file or use a wacky <App />
+*       tag.
+*
+* Main takeaway: This is the first file to be executed in the app.
+*/
 
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom'; //import ReactDOM from the react-dom 3rd party library
+//NOTE: react & react-dom are two dependencies that together form the React library.
 
+import './index.css'; //include the css file in the overall application
+import App from './App'; //App.js; you can & should omit the '.js'
+
+// render takes two arguments...
 ReactDOM.render(<App />, document.getElementById('root'));
+// 1. replace 2nd argument with 1st argument, in this case, App
+// 2 .the div with the 'root' id is where all the code is appended to /public/index.html
